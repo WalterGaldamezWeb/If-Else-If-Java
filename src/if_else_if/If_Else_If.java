ec96aaf73f58fee6135f574843ed2850e62e4248
WalterGaldamezWeb <walter.galdamez.06@gmail.com>
2019-12-27 18:41:25 -0600

    Uso de if else if

---
+++ src/if_else_if/If_Else_If.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if_else_if;
+
+/**
+ *
+ * @author Walter Galdamez
+ */
+public class If_Else_If {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+    
+        if(a==20){
+            System.out.println("a es igual a 20");
+        }else if(a==30){
+            System.out.println("a es igual a 30");
+        }else if(a==40){
+            System.out.println("a es igual a 40");
+        }else{
+            System.out.println("a no es igual quee 20,30 y 40");
+        }
+    }
+    
+}
